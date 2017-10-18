ca05add6fa4b56cc635e3eaad3b9747b4d7aaa8c
Bill Lugo <lugow1@mail.montclair.edu>
2017-10-18 11:07:54 -0400

    Initial commit

---
+++ app/src/main/java/billlugo/boundmessageserviceclient/MainActivity.java
@@ -0,0 +1,18 @@
+package billlugo.boundmessageserviceclient;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void checkMessage(){
+
+        // set up client binder and access AIDL
+    }
+}
